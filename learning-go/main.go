6989c74731c705ce73f32cc1628b20ef59f5970a
OWINY-DOLLO MARVIN <owinymarvin@gmail.com>
2025-10-12 20:39:14 +0300

    finished interfaces

--- learning-go/main.go
+++ learning-go/main.go
@@ -1,45 +1,52 @@
 package main
 
-import "log"
+import "fmt"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberOfTeeth int
+}
 
 func main() {
-	animals := []string{"dog", "cat", "bird", "fish", "horse"}
-	for i, animal := range animals {
-		log.Println(i, animal)
+	dog := Dog{
+		Name:  "Samson",
+		Breed: "German Shepherd",
+	}
+	gorilla := Gorilla{
+		Name:          "Jock",
+		Color:         "Black",
+		NumberOfTeeth: 32,
 	}
 
-	mapedAnimals := make(map[string]string)
-	mapedAnimals["dog"] = "Fideo"
-	mapedAnimals["cat"] = "murphy"
+	PrintInfo(&dog)
+	PrintInfo(&gorilla)
 
-	for mapedAnimalType, mapedAnimal := range mapedAnimals {
-		log.Println(mapedAnimalType, " => ", mapedAnimal)
-	}
+}
 
-	// ranging over a string
-	var firstLine = "Once upon a midnight dreary"
-	log.Println(firstLine)
-	for index, letter := range firstLine {
-		log.Println(index, ":", letter)
-	}
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says ", a.Says(), " and has ", a.NumberOfLegs(), " legs")
+}
 
-	//ranging over a struct
-	type User struct {
-		FirstName string
-		LastName  string
-		Email     string
-		Age       int
-		Password  string
-	}
-	// var users []User
-	var users []User
-	users = append(users, User{"John", "Smith", "XXXXXXXXXXXXXX", 30, "password"})
-	users = append(users, User{"Mary", "Smith", "YYYYYYYYYYYYYY", 25, "password2"})
-	users = append(users, User{"Sally", "Smith", "ZZZZZZZZZZZZZZ", 40, "password3"})
-	users = append(users, User{"Alex", "Smith", "AAAAAAAAAAAAAAA", 45, "password4"})
-	users = append(users, User{"Michael", "Smith", "BBBBBBBBBBBBBBB", 35, "password5"})
-
-	for i, l := range users {
-		log.Println(i, "=>", l.FirstName, l.LastName, l.Email, l.Age, l.Password)
-	}
+func (d *Dog) Says() string {
+	return "Woof! woof!"
+}
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
+func (d *Gorilla) Says() string {
+	return "King Kong"
+}
+func (d *Gorilla) NumberOfLegs() int {
+	return 8
 }
