56d545c3b243df10ddf93b0f7c66851e4775590c
OWINY-DOLLO MARVIN <owinymarvin@gmail.com>
2025-10-12 20:54:03 +0300

    demo of how packages work

--- learning-go/main.go
+++ learning-go/main.go
@@ -1,52 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
-
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+	"github.com/owinymarvin/go_app_udemy/helpers"
+)
 
 func main() {
-	dog := Dog{
-		Name:  "Samson",
-		Breed: "German Shepherd",
-	}
-	gorilla := Gorilla{
-		Name:          "Jock",
-		Color:         "Black",
-		NumberOfTeeth: 32,
-	}
+	var myVar helpers.SomeType
+	myVar.TypeName = "Some name"
+	myVar.TypeNumber = 10
 
-	PrintInfo(&dog)
-	PrintInfo(&gorilla)
-
-}
-
-func PrintInfo(a Animal) {
-	fmt.Println("This animal says ", a.Says(), " and has ", a.NumberOfLegs(), " legs")
-}
-
-func (d *Dog) Says() string {
-	return "Woof! woof!"
-}
-func (d *Dog) NumberOfLegs() int {
-	return 4
-}
-func (d *Gorilla) Says() string {
-	return "King Kong"
-}
-func (d *Gorilla) NumberOfLegs() int {
-	return 8
+	log.Println(myVar.TypeName)
+	log.Println(myVar.TypeNumber)
 }
