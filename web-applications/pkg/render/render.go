09449ff02b717f2c42f4932e28ee945e7b40b719
OWINY-DOLLO MARVIN <owinymarvin@gmail.com>
2025-10-14 20:05:32 +0300

    added base templates

--- web-applications/pkg/render/render.go
+++ web-applications/pkg/render/render.go
@@ -8,7 +8,7 @@ import (
 
 // make it start with an upercase letter to make it publicly available
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, err := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html")
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		fmt.Println("Error parsing the template:", err)
