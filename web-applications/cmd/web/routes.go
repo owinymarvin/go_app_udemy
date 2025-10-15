5a7590ecc0a716897e39f0ec02a6b7d34b29d12c
Owiny Marvin <owinymarvin@gmail.com>
2025-10-15 14:19:23 +0300

    swapped pat router for chi router with middleware

--- web-applications/cmd/web/routes.go
+++ web-applications/cmd/web/routes.go
@@ -3,14 +3,22 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/owinymarvin/go_app_udemy/pkg/config"
 	"github.com/owinymarvin/go_app_udemy/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.Home)
 	return mux
 }
--- web-applications/go.mod
+++ web-applications/go.mod
@@ -2,4 +2,4 @@ module github.com/owinymarvin/go_app_udemy
 
 go 1.25.1
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi/v5 v5.2.3
